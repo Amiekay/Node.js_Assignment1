40e2bb9265534b2cb7b01daab9124308a95dce87
Amiekay <amara.kaluagu@gmail.com>
2023-08-05 12:19:24 -0400

    Update

--- FS_module.js
+++ FS_module.js
@@ -1,25 +1,91 @@
-// Create directory/folder named: “Students”
+const fs = require('fs');
+const path = require('path');
 
+// Create directory/folder named: “Students”
+fs.mkdir('./Students', (err) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('Folder created succesfully')
+})
 
 // In the Students directory, create a file named user.js
+const newFile = path.join(__dirname, 'Students', 'user.js');
+const content = "'User details: ";
 
+fs.writeFile(newFile, content, (err) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('File created successfully')
+})
 
 // Update the Students directory to “Names”
+const currentFolder = './Students'
+const newFolderName = './Names'
 
-
+fs.rename(currentFolder, newFolderName, function(err) {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('Successfully renamed folder')
+})
 // Add your name as content to the file user.js
+const filePath = path.join(__dirname, "./Names", "user.js")
 
+fs.appendFile(filePath, 'Kalu Amarachi', (err) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('Name appended successfully')
+})
 
 // Update the file and add your age, sex, nationality, phone number and any other information about yourself
-
-
+const additionalInfo = '\n 29yrs old, \n Female, \n Nigerian, \n 08131559061';
+fs.appendFile(filePath, 'Kalu Amarachi', additionalInfo ,(err) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('Info appended successfully')
+}) 
 // Update the file user.js to {your_name}.js eg daniel_adesoji.js
-
+const newFilePath = path.join(__dirname, "Names", "Kalu_Amarachi.js")
+fs.rename(filePath, newFilePath, function(err) {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('Successfully renamed folder')
+})
 
 // Read the contents from {your_name}.js. User fs.open or fs.readFile
 
-
+fs.readFile(newFilePath, (err)=>{
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('File read succesfully')
+})
 // Delete the file {your_name}.js
-
+fs.rm(newFilePath,(err) =>{
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('file deleted successfully')
+})
 
 // Delete the directory “Names”
+fs.rmdir(newFolderName, (err) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('folder deleted successfully')
+})
--- HTTP_module.js
+++ HTTP_module.js
@@ -1,6 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const bookPath = path.join(__dirname, )
 // Create an http server
+const HOST_NAME = "127.0.0.1"
+const PORT = "8000"
+function handleRequest(req, res){
+postBooks(req,res)
+}
+const server = http.createServer(handleRequest)
+server.listen(PORT, HOST_NAME, ()=> {
+    console.log(`server has started running at http://${PORT}:${HOST_NAME}`)
+})
+function postBooks(req,res){
+   const body = []
+    req.on('data', (chunk) => {
+        body.push(chunk)
+    })
+    req.on('end', ()=> {
+        const parsedBody = Buffer.concat(body).toString()
+        const booksToPost = JSON.parse(parsedBody)
+        fs.writeFile(bookPath, JSON.stringify(booksToPost), (err)=>{
+            if(err){
 
+        }
 
+        })
+    })
+}
 // Return “Hello world” from the response
 
 
